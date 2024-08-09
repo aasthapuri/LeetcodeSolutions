0529199036f75715927fb843fd6d898276635284
Aastha Puri <aasthapuri28@gmail.com>
2024-08-09 17:04:38 -0400

    Time: 26 ms (6.98%) | Memory: 52.4 MB (86.15%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        Map<Integer,Integer> map=new HashMap<>();
+        if(n==1) return 1;
+        for(int[] edge:trust){
+            map.put(edge[0],map.getOrDefault(edge[0],0)-1);
+            map.put(edge[1],map.getOrDefault(edge[1],0)+1);
+        }
+
+
+        for(int node:map.keySet()){
+            if(map.get(node)==n-1){
+                return node;
+            }
+        }
+        return -1;
+    }
+}
