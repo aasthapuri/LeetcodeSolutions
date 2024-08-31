aa76442bd6c7e9b497be51e4694060d7c8e264ac
Aastha Puri <aasthapuri28@gmail.com>
2024-08-31 17:07:38 -0400

    Time: 4 ms (17.31%) | Memory: 61 MB (60.08%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -7,46 +7,31 @@ class Solution {
             min = Math.min(min,nums[i]);
             max = Math.max(max,nums[i]);
         }
-        int leftcount = 0;
         
-        boolean minimum = false;
-        boolean maximum = false;
-        
-        int i = 0;
-        while(i<nums.length && (!minimum || !maximum)){
-            if(nums[i]==min){
-                minimum = true;
-            }
-            if(nums[i]==max){
-                maximum = true;
-            }
-            leftcount++;
-            i++;
-        }
         int j = nums.length-1;
-        minimum = false;
-        maximum = false;
-        int rightcount = 0;
+       
         int minIndex = -1;
         int maxIndex = -1;
-        while(j>=0 && (!minimum || !maximum)){
+        while(j>=0){
             if(nums[j]==min){
-                minimum = true;
                 minIndex = j;
             }
             if(nums[j]==max){
-                maximum = true;
                 maxIndex = j;
             }
-            rightcount++;
             j--;
         }
-        int index = Math.max(minIndex,maxIndex);
-        int index2 = Math.min(minIndex,maxIndex);
-        int index3 = index2+nums.length-index+1;
-        
-        int minimumCount = Math.min(leftcount,rightcount);
-        int minCount = Math.min(minimumCount,index3);
-        return minCount;
+        if(minIndex<maxIndex){
+            int p = maxIndex + 1;
+            int p1 = nums.length - minIndex;
+            int p2 = minIndex + 1 + nums.length - maxIndex;
+            return Math.min(p,Math.min(p1,p2));
+        }
+        else{
+            int p = minIndex + 1;
+            int p1 = nums.length - maxIndex;
+            int p2 = maxIndex + 1 + nums.length - minIndex;
+            return Math.min(p,Math.min(p1,p2));
+        }
     }
 }
