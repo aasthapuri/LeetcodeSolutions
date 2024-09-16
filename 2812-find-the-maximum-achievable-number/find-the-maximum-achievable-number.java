ca425b1cc4d25953c602b947c4dd158fcf90c4c8
Aastha Puri <aasthapuri28@gmail.com>
2024-09-15 21:10:04 -0400

    Time: 1 ms (100.00%) | Memory: 41.4 MB (57.07%) - LeetSync

--- 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.java
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
-        t = 2*t;
-        num = num + t;
+        
+        num = num + t*2;
         
         return num;
     }
