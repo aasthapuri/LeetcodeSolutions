efc6410a49b04c4dcf1f2b1586ab3c76f8d55561
Aastha Puri <aasthapuri28@gmail.com>
2024-09-27 17:29:52 -0400

    Time: 1009 ms (5.00%) | Memory: 121.4 MB (23.97%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,37 +1,36 @@
 class LRUCache {
-    Deque<Integer> queue;
-    HashMap<Integer,Integer> map = new HashMap<>();
+    Map<Integer,Integer> map;
+    Deque<Integer> q;
     int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        queue = new LinkedList<>();
         map = new HashMap<>();
+        q = new LinkedList<>();
     }
     
     public int get(int key) {
         if(!map.containsKey(key)){
             return -1;
         }
-        queue.remove(key);
-        queue.addLast(key);
-        
+        int value = map.get(key);
+        q.remove(key);
+        q.addLast(key);
         return map.get(key);
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            queue.remove(key);
+            q.remove(key);
             map.remove(key);
         }
         map.put(key,value);
-        queue.addLast(key);
+        q.addLast(key);
         
         if(map.size()>capacity){
-            int remove = queue.pollFirst();
+            int remove = q.pollFirst();
             map.remove(remove);
         }
-        
     }
 }
 
